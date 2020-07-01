fa58eb80f93ff94fc7f6f0555b45bc06f42e0dcd
Jay-programmer <polanemahloko@gmail.com>
2020-07-01 03:12:44 +0200

    added a c++ file

---
+++ tester.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<fstream>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+	int numbers=100;
+	
+	cout<<"You have "<<numbers<<" manuals available."<<endl;
+	
+	return 0;
+	}
